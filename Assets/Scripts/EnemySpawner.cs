2f9c26040e1cbfc7d81137681dacc3b58f564928
Daniel <Sychkov.Daniil@yandex.ru>
2023-07-13 22:45:58 +0300

    SpawnerUpdate
    moved spawn logic to a parent spawner object, child objects responsibility reduced to spawn points; increased typization of enemy template

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,11 +1,50 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _enemyTemplate;
+    [SerializeField] private Enemy _template;
+    [SerializeField] private float _sleepDuration;
 
-    public void Spawn()
+    private SpawnPoint[] _spawnPoints;
+    private WaitForSeconds _sleep;
+    private int _currentSpawnerNumber = 0;
+
+    private bool _isSpawning;
+
+    private void OnValidate()
     {
-        Instantiate(_enemyTemplate, transform.position, transform.rotation);
+        
+    }
+
+    private void Awake()
+    {
+        _spawnPoints = GetComponentsInChildren<SpawnPoint>();
+        _sleep = new WaitForSeconds(_sleepDuration);
+    }
+
+    private void OnEnable()
+    {
+        _isSpawning = true;
+        StartCoroutine(SpawnEnemiesCoroutine());
+    }
+
+    private void OnDisable()
+    {
+        _isSpawning = false;
+    }
+
+    private IEnumerator SpawnEnemiesCoroutine()
+    {
+        while (_isSpawning)
+        {
+            Transform enemyTransform = _spawnPoints[_currentSpawnerNumber].transform;
+            Instantiate(_template, enemyTransform.position, enemyTransform.rotation);
+
+            if (++_currentSpawnerNumber >= _spawnPoints.Length)
+                _currentSpawnerNumber = 0;
+
+            yield return _sleep;
+        }
     }
 }
